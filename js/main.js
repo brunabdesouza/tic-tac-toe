f535ba1280f8b19161d7056f8fd1125e6b200cc2
Bruna Barbosa de Souza <bruna.bdesouza@gmail.com>
2020-11-16 15:13:53 +1100

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log('Tic Tac Toe');
+
+// Your app must:
+//
+// Render a game board in the browser
+// Switch turns between X and O (or whichever markers you select); your game should prevent users from playing a turn into a square that is already occupied
+// Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
+// Include separate HTML / CSS / JavaScript files
+// Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
+// Use Javascript with jQuery for DOM manipulation
+// Deploy your game online, where the rest of the world can access it
+// Use semantic markup for HTML and CSS (adhere to best practices)
