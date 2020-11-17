c255c629923c96fc6da7b3bd12a9957133a066f5
Bruna Barbosa de Souza <bruna.bdesouza@gmail.com>
2020-11-17 17:28:09 +1100

    update

--- js/main.js
+++ js/main.js
@@ -10,3 +10,60 @@ console.log('Tic Tac Toe');
 // Use Javascript with jQuery for DOM manipulation
 // Deploy your game online, where the rest of the world can access it
 // Use semantic markup for HTML and CSS (adhere to best practices)
+
+const boardGame = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i'],
+  ['a', 'd', 'g'],
+  ['b', 'e', 'h'],
+  ['c', 'f', 'i'],
+  ['a', 'e', 'i'],
+  ['g', 'e', 'c']
+];
+
+
+let currentPlayer = 'X';
+
+$(document).ready(function() {
+
+  $('.board').on('click', function() {
+
+    // console.log('clicked');
+    $(this).html(currentPlayer);
+
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O';
+    } else {
+      currentPlayer = 'X'
+    } //end of if(currentPlayer)
+
+    //check to see if someone has won
+    if ($('#box1').html() === 'X' && $('#box2').html() === 'X' && $('#box3').html() === 'X') {
+      console.log('X Wins!');
+    }
+    if ($('#box1').html() === 'O' && $('#box2').html() === 'O' && $('#box3').html() === 'O') {
+      console.log('O Wins!');
+
+    }
+
+    if ($('#box4').html() === 'X' && $('#box5').html() === 'X' && $('#box6').html() === 'X') {
+      console.log('X Wins!');
+    }
+
+    if ($('#box4').html() === 'O' && $('#box5').html() === 'O' && $('#box6').html() === 'O') {
+      console.log('O Wins!');
+    }
+
+    if ($('#box7').html() === 'X' && $('#box8').html() === 'X' && $('#box9').html() === 'X') {
+      console.log('X Wins!');
+    }
+
+    if ($('#box7').html() === 'O' && $('#box8').html() === 'O' && $('#box9').html() === 'O') {
+      console.log('O Wins!');
+    }
+
+  }); //end of click handler
+
+
+}); //end of $(document).ready(function()).
